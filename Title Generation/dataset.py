c166d57c68793d243b348b47fb4d19dbdd3108c8
Costanza Laudisa <c.laudisa@students.uu.nl>
2021-01-04 13:42:39 +0100

    Fixed genre split in dataset

--- Title Generation/dataset.py
+++ Title Generation/dataset.py
@@ -3,38 +3,37 @@ from imports import *
 def prepare_dataset():
     # Load dataset
     df = pd.read_csv('wiki_movie_plots_deduped.csv')
+    print("Original dataset shape: ", df.shape)
 
     # Remove movies with 'unknown' and '-' genres
     df = df[df['Genre'] != 'unknown']
     df = df[df['Genre'] != '-']
 
-    # Remove parts in brackets
+    # Remove parts in brackets (some genres contain [num] or [not in citation given], others contain irrelevant details in brackets)
     df['Genre'] = df['Genre'].str.replace("[\(\[].*?[\)\]]", "")
 
-    # Handle genres (split by / - , ; etc.)
-    new_df = pd.DataFrame(df['Genre'].str.split('/').tolist(), index=df['Title']).stack()
-    new_df = new_df.reset_index([0, 'Title'])
-    new_df.columns = ['Title', 'Genre']
-    df = new_df
+    # Split multiple genres by / - , ; & etc.
+    df['Genre'] = df['Genre'].str.split("/")
+    df = df.explode('Genre').reset_index(drop=True)
 
-    new_df = pd.DataFrame(df['Genre'].str.split(' - ').tolist(), index=df['Title']).stack()
-    new_df = new_df.reset_index([0, 'Title'])
-    new_df.columns = ['Title', 'Genre']
-    df = new_df
+    df['Genre'] = df['Genre'].str.split(" - ")
+    df = df.explode('Genre').reset_index(drop=True)
 
-    new_df = pd.DataFrame(df['Genre'].str.split(',').tolist(), index=df['Title']).stack()
-    new_df = new_df.reset_index([0, 'Title'])
-    new_df.columns = ['Title', 'Genre']
-    df = new_df
+    df['Genre'] = df['Genre'].str.split("&")
+    df = df.explode('Genre').reset_index(drop=True)
 
-    new_df = pd.DataFrame(df['Genre'].str.split(';').tolist(), index=df['Title']).stack()
-    new_df = new_df.reset_index([0, 'Title'])
-    new_df.columns = ['Title', 'Genre']
-    df = new_df
+    df['Genre'] = df['Genre'].str.split(",") # conflict with genre that contains '4,000'
+    df = df.explode('Genre').reset_index(drop=True)
 
-    df['Genre'] = df['Genre'].str.strip() # trim strings after split
+    df['Genre'] = df['Genre'].str.split(";")
+    df = df.explode('Genre').reset_index(drop=True)
+
+    # Trim strings after split
+    df['Genre'] = df['Genre'].str.strip()
 
     # Drop movies with no genre
     df = df[df['Genre'] != '']
+
+    print("Final dataset shape: ", df.shape)
 
     return df
--- Title Generation/main.py
+++ Title Generation/main.py
@@ -1,11 +1,13 @@
 from imports import *
 from dataset import prepare_dataset
-
-# 
+ 
 # print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
+# Gather dataset
 df = prepare_dataset()
 print(df)
 
+# Define and sort genre list
 genres = df['Genre'].unique()
-print("Length of genre list:", len(genres), "  |   Genre list: ", genres)
+sorted_genres = sorted(genres)
+print("Length of genre list:", len(sorted_genres), "  |   Genre list: ", sorted_genres)
